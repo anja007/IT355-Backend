309e65830e33c0619c2552a15582439277e2f477
anja007 <popovicanja972@gmail.com>
2025-07-31 23:46:21 +0200

    Made minor changes in generating token

--- src/main/java/com/example/CS330_PZ/security/JwtTokenProvider.java
+++ src/main/java/com/example/CS330_PZ/security/JwtTokenProvider.java
@@ -16,7 +16,7 @@ public class JwtTokenProvider {
 
     @Value("${jwt.expiration-ms}")
     private int jwtExpirationMs;
-
+/*
     public String generateToken(Authentication authentication) {
         Date now = new Date();
 
@@ -26,7 +26,24 @@ public class JwtTokenProvider {
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
                 .compact();
-    }
+    }*/
+public String generateToken(Authentication authentication) {
+    Date now = new Date();
+
+    String username = authentication.getName();
+    String role = authentication.getAuthorities().stream()
+            .findFirst()
+            .map(grantedAuthority -> grantedAuthority.getAuthority().replace("ROLE_", ""))
+            .orElse("USER");
+
+    return Jwts.builder()
+            .setSubject(username)
+            .claim("role", role) 
+            .setIssuedAt(now)
+            .setExpiration(new Date(now.getTime() + jwtExpirationMs))
+            .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+            .compact();
+}
 
     public String getUsernameFromJwt(String token) {
         return Jwts.parserBuilder()
