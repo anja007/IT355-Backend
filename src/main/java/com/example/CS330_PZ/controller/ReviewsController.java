6038ba094acabd7b626d01ff81cfd45996872445
anja007 <popovicanja972@gmail.com>
2025-08-02 01:42:50 +0200

    Added @PreAuthorize

--- src/main/java/com/example/CS330_PZ/controller/ReviewsController.java
+++ src/main/java/com/example/CS330_PZ/controller/ReviewsController.java
@@ -5,6 +5,7 @@ import com.example.CS330_PZ.model.Reviews;
 import com.example.CS330_PZ.service.ReviewsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +17,12 @@ import java.util.List;
 public class ReviewsController {
     private final ReviewsService reviewsService;
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<?> getAllReviews(){
         return ResponseEntity.ok(reviewsService.getAllReviews());
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/myReviews")
     public ResponseEntity<List<Reviews>> getMyReviews(Authentication authentication) {
         String username = authentication.getName();
@@ -29,11 +31,13 @@ public class ReviewsController {
         return ResponseEntity.ok(reviews);
     }
 
+    /*
     @GetMapping("/review/{id}")
     public ResponseEntity<?> getReviewById(@PathVariable Long id) {
         return ResponseEntity.ok(reviewsService.getReviewById(id));
-    }
-    
+    }*/
+
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/createReview")
     public ResponseEntity<?> createReview(@RequestBody ReviewsDTO dto){
         return ResponseEntity.ok(reviewsService.createReviews(dto));
@@ -44,11 +48,13 @@ public class ReviewsController {
         return ResponseEntity.ok(reviewsService.getReviewsByPlaceId(placeId));
     }
 
+    @PreAuthorize("hasRole('USER')")
     @PutMapping("/{id}")
     public ResponseEntity<?> updateReview(@PathVariable Long id, @RequestBody ReviewsDTO dto){
         return ResponseEntity.ok(reviewsService.updateReview(id, dto));
     }
 
+    @PreAuthorize("hasRole('USER')")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteReview(@PathVariable Long id) {
         boolean deleted = reviewsService.deleteReview(id);
