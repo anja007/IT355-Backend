f83de3403f5fdb1a6f72e229a21fb02e447018f8
anja007 <popovicanja972@gmail.com>
2025-08-06 16:11:02 +0200

    Added integration test

---
+++ src/test/java/com/example/CS330_PZ/integrationTests/ReviewsControllerIntegrationTests.java
@@ -0,0 +1,24 @@
+package com.example.CS330_PZ.integrationTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReviewsControllerIntegrationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetEndpoint() throws Exception {
+        mockMvc.perform(get("/api/reviews/all"))
+                .andExpect(status().isOk());
+    }
+}
